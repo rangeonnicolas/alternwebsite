5112eca84dea08bcfa9359b73893f070f314689e
NicolasRangeonRaccourci <nicolas.rangeon@raccourci.fr>
2015-07-07 14:24:15 +0200

    models

--- api/models/alternative.py
+++ api/models/alternative.py
@@ -1,1 +1,5 @@
 __author__ = 'developpeur'
+
+
+class Alternative:
+    pass
--- api/models/object.py
+++ api/models/object.py
@@ -1,1 +1,6 @@
 __author__ = 'developpeur'
+from django.db import models
+
+class Object:
+    updated = models.DateTimeField(auto_created= True)
+    created = models.DateTimeField(auto_now_add= True)
