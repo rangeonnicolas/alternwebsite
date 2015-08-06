c8636227410652a8e9c0847fa71a092f5d8e9272
Nicolas Rangeon <nicapps23@gmail.com>
2015-08-06 13:29:14 +0200

    formulaire email

--- altws/views.py
+++ altws/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from altws.forms import ContactForm
 
 def home(request):
+
+    if request.method == 'POST':  # S'il s'agit d'une requête POST
+        form = ContactForm(request.POST)  # Nous reprenons les données
+
+        if form.is_valid(): # Nous vérifions que les données envoyées sont valides
+
+            # Ici nous pouvons traiter les données du formulaire
+            form.save()
+
+            # Nous pourrions ici envoyer l'e-mail grâce aux données que nous venons de récupérer
+
+            ok = True
+
+    else: # Si ce n'est pas du POST, c'est probablement une requête GET
+        form = ContactForm()  # Nous créons un formulaire vide
+
 
     return render(request, 'home.html', locals())
 
---
+++ maquette/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='mails',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('email', models.EmailField(max_length=254)),
+            ],
+        ),
+    ]
