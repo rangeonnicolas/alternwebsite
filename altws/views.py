2a085c8b49bc8671e6d27c9a1e3893ae8b64a917
Nicolas Rangeon <nicapps23@gmail.com>
2015-08-06 14:08:00 +0200

    test envoi email

--- altws/views.py
+++ altws/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from altws.forms import ContactForm
 
+import smtplib
+import datetime
+
 def home(request):
 
     if request.method == 'POST':  # S'il s'agit d'une requête POST
@@ -14,6 +17,34 @@ def home(request):
             # Nous pourrions ici envoyer l'e-mail grâce aux données que nous venons de récupérer
 
             ok = True
+
+
+
+
+
+
+            #sender = 'ubuntu@ec2-54-187-124-250.us-west-2.compute.amazonaws.com'
+            sender = 'ubuntu@openalternativa.com'
+            receivers = ['nicapps23@gmail.com']
+
+            
+            message = """
+            il est tres exactement :
+            """ + str(datetime.datetime.now())
+
+            try:
+               smtpObj = smtplib.SMTP('localhost')
+               smtpObj.sendmail(sender, receivers, message)
+               print "Successfully sent email"
+            except smtplib.SMTPException:
+               print "Error: unable to send email"
+
+
+
+
+
+
+
 
     else: # Si ce n'est pas du POST, c'est probablement une requête GET
         form = ContactForm()  # Nous créons un formulaire vide
