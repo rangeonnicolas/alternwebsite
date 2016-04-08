d68a30423d40b8642ed26232c1ca615bcf58c9c1
Nicolas R <nicapps23@gmail.com>
2016-04-07 21:37:51 -0500

    change setting database

--- altws/settings.py
+++ altws/settings.py
@@ -59,10 +59,19 @@ WSGI_APPLICATION = 'altws.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'altws',
+        'USER': 'django',
+        'PASSWORD': 'mypassword1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 # Internationalization
